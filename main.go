ba6a57a7eda93980f47c66a2f160bfcef62285c9
arincantikam26 <arincantikam@gmail.com>
2022-06-27 08:47:24 +0700

    deploy heroku

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"database/sql"
+
+	"github.com/arincantikam26/volunteeredu/api"
+	"github.com/arincantikam26/volunteeredu/repository"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "database/VolunteerEdu.db")
+	if err != nil {
+		panic(err)
+	}
+
+	usersRepo := repository.NewUserRepository(db)
+	classRepo := repository.NewClassRepository(db)
+	activityRepo := repository.NewActivityRepository(db)
+	galleryRepo := repository.NewGalleryRepo(db)
+	roleRepo := repository.NewRoleRepository(db)
+
+	mainAPI := api.NewAPI(*usersRepo, *classRepo, *activityRepo, *galleryRepo, *roleRepo)
+
+	mainAPI.Start()
+
+	// router.Use(cors.Default())
+
+}
